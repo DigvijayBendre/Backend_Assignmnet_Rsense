105a6434f66fa496fdd5e3467518a325cbac56b5
Digvijay Bendre <116354441+DigvijayBendre@users.noreply.github.com>
2023-04-13 20:35:58 +0530

    Update employee.go

--- Rcurd/employee.go
+++ Rcurd/employee.go
@@ -2,6 +2,7 @@ package main
 
 // Employee is a struct that represents an employee.
 type Employee struct {
+	ID     uint   `gorm:"primaryKey;autoIncrement" json:"id"`
 	Name   string `json:"name"`
 	Email  string `json:"email"`
 	Dob    string `json:"dob"`
